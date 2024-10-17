3cc76024be903f30238897a3e571ca1061dfe934
KeyToFailure <juliagirlhehe@gmail.com>
2024-10-16 21:14:48 -1000

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,1 +1,14 @@
-loads
+local placeId = game.PlaceId
+
+local function getGamePlaceId()
+  if placeId then
+    return placeId
+  end
+  return nil
+end
+
+local suc, err = pcall(function()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/TremnDevelopment/ScriptVault/refs/heads/main/" .. getGamePlaceId() .. ".lua"))()
+end)
+
+if not suc then error(err) end
